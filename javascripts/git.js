5e832172073ea854f0ee6e0d07127dd10b892ca5
bsyu1125 <brianyu1125@gmail.com>
2015-06-30 15:15:54 -0700

    fixed issues

---
+++ javascripts/git.js
@@ -0,0 +1,19 @@
+jQuery.githubUser = function(username, callback) {
+   jQuery.getJSON('https://api.github.com/orgs/'+username+'/members?callback=?',callback)
+}
+ 
+jQuery.fn.loadRepositories = function(username) {
+    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
+     
+    var target = this;
+    $.githubUser(username, function(data) {
+        var repos = data.data; // JSON Parsing
+        //sortByName(repos);    
+     
+        var list = $('<dl/>');
+        target.empty().append(list);
+        $(repos).each(function() {
+                list.append('<dt>' + list.id);
+        });      
+      });
+};
